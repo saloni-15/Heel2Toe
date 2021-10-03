25835704b6ef1f99e50c92777bd5a7f41b348221
nurulramadhona <nrdhona@gmail.com>
2021-10-03 10:17:23 +0700

    directing url to id page (only applies to home page)

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ btn.on('click', function (e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+// Direct URL to ID (only applies to home page)
+
+if ($('div').hasClass('wrapper')) {
+    $('a[href="index.html"]').prop('href', '#');
+} else {
+    $('a[href="index.html"]').prop('href', 'index.html');
+}
+
+if ($('div').hasClass('wrapper')) {
+    $('a[href="men.html"]').prop('href', 'index.html#men');
+} else {
+    $('a[href="men.html"]').prop('href', 'men.html');
+}
+
+if ($('div').hasClass('wrapper')) {
+    $('a[href="women.html"]').prop('href', 'index.html#women');
+} else {
+    $('a[href="women.html"]').prop('href', 'women.html');
+}
+
+if ($('div').hasClass('wrapper')) {
+    $('a[href="arrivals.html"]').prop('href', 'index.html#arrivals');
+} else {
+    $('a[href="arrivals.html"]').prop('href', 'arrivals.html');
+}
+
+if ($('div').hasClass('wrapper')) {
+    $('a[href="trending.html"]').prop('href', 'index.html#trending');
+} else {
+    $('a[href="trending.html"]').prop('href', 'trending.html');
+}
+
+if ($('div').hasClass('wrapper')) {
+    $('a[href="best.html"]').prop('href', 'index.html#best');
+} else {
+    $('a[href="best.html"]').prop('href', 'best.html');
+}
