445f26be6414095b90e559874630b4a2a1924a99
Yogesh <52989607+yogesh2k21@users.noreply.github.com>
2021-10-15 10:08:01 +0530

    Scroll update1

--- script.js
+++ script.js
@@ -38,4 +38,4 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
-}
+};
